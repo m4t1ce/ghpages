10e5a20d310fb6cbd93c606728cbb1abd0893d15
m4t1ce <m4t1ce@gmail.com>
2020-01-11 23:48:33 +0100

    Updates

--- src/components/Projects.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Projects extends React.Component {
-    render() {
-      return <h2>Hi, I am a Projects!</h2>;
-    }
-  }
-
-export default Projects
