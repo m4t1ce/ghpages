a153788757f6d2079e927f32e48619ba54e665c5
m4t1ce <m4t1ce@gmail.com>
2020-01-27 22:20:50 +0100

    Cards/no data

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,19 @@
 import React from "react"
-// import Card from 'react-bootstrap/Card'
-// import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 class Project extends React.Component {
     render() {
         return(
-          <div>
-            {/* <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '18rem',marginBottom: '1vh',backgroundColor:'rgb(41,44,47)' }}>
             <Card.Img variant="top" src="holder.js/100px180" />
             <Card.Body>
                 <Card.Title>{this.props.project.title}</Card.Title>
                 <Card.Text>
                 {this.props.project.description}
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="dark">Go somewhere</Button>
             </Card.Body>
-            </Card> */}
-          </div>
+            </Card>
         )
     }
   }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import data from '../data/data'
 import Project from './Project'
+import CardColumns from 'react-bootstrap/CardColumns'
 class Projects extends React.Component {
     render() {
 
-      return ( <div>
-          <div>
-                    {
+      return ( 
+      <div className="Projects">
+      <CardColumns>
+                {
                     data.projects.map(project =>{
-                        return <Project key={project.title} project={project}/>
-                    })
-                    }
-                </div>
+                    return <Project key={project.title} project={project}/>
+                })
+                }
+      </CardColumns>
       </div>
       )
   }}
