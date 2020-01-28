83e538f54e5410e24c62e46595ec7e2a39258d02
m4t1ce <m4t1ce@gmail.com>
2020-01-28 21:28:43 +0100

    added projects to data.json and styled cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,14 +4,14 @@ import Button from 'react-bootstrap/Button'
 class Project extends React.Component {
     render() {
         return(
-            <Card style={{ width: '18rem',marginBottom: '1vh',backgroundColor:'rgb(41,44,47)' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card style={{ width: '30vw',height:'40vh',marginBottom: '1vh',backgroundColor:'rgb(41,44,47)' }}>
+            {/* <Card.Img variant="top" src={this.props.project.img} /> */}
             <Card.Body>
                 <Card.Title>{this.props.project.title}</Card.Title>
-                <Card.Text>
+                <Card.Text style={{fontSize: '1.3vw'}}>
                 {this.props.project.description}
                 </Card.Text>
-                <Button variant="dark">Go somewhere</Button>
+                <Button variant="dark" href={this.props.project.link}>Navigate to site</Button>
             </Card.Body>
             </Card>
         )
